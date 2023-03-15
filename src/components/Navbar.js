0b140b524f57daf17cfd0fa1f9c2153f27fae8bd
Marko Kermiciev <kermicievmarko@gmail.com>
2023-03-15 23:09:13 +0100

    setup redux folder & conecting with API

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { NavLink, Outlet, useLocation } from 'react-router-dom';
+import { IoChevronBack } from 'react-icons/io5';
+import styles from '../styles/Navbar.module.css';
+
+const Navbar = () => {
+  const location = useLocation();
+
+  return (
+    <div className={styles.navbar}>
+      <NavLink to="/" className={styles.link}>
+        {location.pathname !== '/' && <IoChevronBack className={styles.back} />}
+      </NavLink>
+      <Outlet />
+    </div>
+  );
+};
+
+export default Navbar;
